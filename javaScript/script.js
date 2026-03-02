34d67023a771e2f50a401fb71758d1fb6ddc67f3
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-02 15:46:52 +0500

    basic code of javascript

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,8 +1,22 @@
 console.log('this is my first class')
-Name = "enc"
-console.log(Name)
+// Name = "enc"
+// console.log(Name)
+// 
+// alert("welcome to my first class")
+// document.write("welcome to my first class")
+// const name = prompt("what is your name")
+// console.log(name)
 
-alert("welcome to my first class")
-document.write("welcome to my first class")
-const name = prompt("what is your name")
-console.log(name)
+let marks = prompt("what is your marks")
+
+if(marks > 90){
+    console.log("you got A+")
+}else if(marks > 80){
+    console.log("you got A")
+}else if(marks > 70){
+    console.log("you got B")
+}else if(marks > 60){
+    console.log("you got C")
+}else if(marks < 50){
+    console.log("you are fail")
+}
