29e01775b158e8ca56587915534b01f497b6e20a
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-02 15:41:11 +0500

    basic code of javascript

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,3 +1,8 @@
 console.log('this is my first class')
 Name = "enc"
 console.log(Name)
+
+alert("welcome to my first class")
+document.write("welcome to my first class")
+const name = prompt("what is your name")
+console.log(name)
