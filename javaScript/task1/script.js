6cf9c2082cc567f5c5ac5aa9858783defb7f02fc
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-04 14:49:45 +0500

    javascript 3rd class

---
+++ javaScript/task1/script.js
@@ -0,0 +1,18 @@
+const persons = [
+    {
+        imgUrl: "https://media.licdn.com/dms/image/C4D03AQH8n9sXo2l7w/profile-displayphoto-shrink_800_800/0/1516909134415?e=1701302400&v=beta&t=Zt3m1a9j8h6qjLh7kKJQGg5u2n1sXoVb3z5c8f9s",
+        name: "John",
+        jobTitle: "Senior fulls stack web engineer",
+        description: "Tupelo, Mississippi, United States",
+        connection: 184
+    },
+    {
+        imgUrl: "https://thisIsUrl.com",
+        name: "ibrahim",
+        jobTitle: "Jonior fulls stack web engineer",
+        description: "Tupelo, Mississippi, United States",
+        connection: 381
+    }
+]
+
+console.log(persons[1].name);
