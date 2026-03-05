9d33a9233f12549d4296b8285b728a166f14a3be
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-05 12:00:00 +0500

    print no from 10 to 1

--- javaScript/activity-base-task/script.js
+++ javaScript/activity-base-task/script.js
@@ -76,8 +76,18 @@
 //     console.log("The number is odd");
 // }
 
-for (let i = 0; i <= 10; i++) {
-    if (i % 2 === 0) {
-        console.log(i)
-    }
+
+// *****************************************
+// print even numbers from 0 to 10
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// *****************************************
+// print number from 10 to 0 
+for (let i = 10; i > 0; i++) {
+    console.log(i)
 }
+
