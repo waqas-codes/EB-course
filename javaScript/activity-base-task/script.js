d4d5578d6fb9ebca196996bc46d2155641286ed7
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-06 14:33:25 +0500

    guess number with while loop

--- javaScript/activity-base-task/script.js
+++ javaScript/activity-base-task/script.js
@@ -1,17 +1,17 @@
-// let x = Number(prompt("Enter a number"));
-
-// while (x !== 5) {
-//     x = Number(prompt("Enter a number"));
-//     console.log("Number is incorrect! try again")
-// }
-
-// console.log("congratulations! Number is correct!");
-
 let x = Number(prompt("Enter a number"));
 
-while (x % 5 !== 0) {
+while (x !== 5) {
     x = Number(prompt("Enter a number"));
-    console.log("The number is not divisible by 5")
+    console.log("Number is incorrect! try again")
 }
 
-console.log("The number is divisible by 5!")
+console.log("congratulations! Number is correct!");
+
+// let x = Number(prompt("Enter a number"));
+
+// while (x % 5 !== 0) {
+//     x = Number(prompt("Enter a number"));
+//     console.log("The number is not divisible by 5")
+// }
+
+// console.log("The number is divisible by 5!")
