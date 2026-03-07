608f03440b38fd5d49b6b4dccb51ced0e5e779f8
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-07 12:00:00 +0500

    DOM

--- javaScript/events/script.js
+++ javaScript/events/script.js
@@ -1,12 +1,18 @@
-const btn = document.getElementsByTagName("button")
-btn[0].addEventListener("click", () => {
+const btn1 = document.getElementById("btn1")
+const btn2 = document.getElementById("btn2")
+const ol = document.getElementsByTagName("ol")
+const lis = document.createElement("li")
+    
+btn1.addEventListener("click", () => {
     const container = document.getElementsByTagName("div")
     container[0].style.backgroundColor = "red"
     const para = document.createElement("p")
     para.innerText = "i am a prargraph"
     container[0].appendChild(para)
-    const li = document.createElement("li")
-    li.innerText = "item 4"
-    const ol = document.getElementsByTagName("ol")
-    ol[0].appendChild(li)
+    lis.innerText = "item 4"
+    ol[0].appendChild(lis)
+})
+
+btn2.addEventListener("click", () => {
+    ol[0].removeChild(lis)
 })
