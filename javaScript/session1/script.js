fc1164e6a4c90a3ee9f62a0d1c227528a6d08d9e
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-09 15:37:29 +0500

    js session

---
+++ javaScript/session1/script.js
@@ -0,0 +1,29 @@
+// let num1 = Number(prompt("Enter 1st number"))
+// let num2 = Number(prompt("Enter 2nd number"))
+
+// console.log(num1+num2)
+// console.log(num1-num2)
+// console.log(num1*num2)
+// console.log(num1%num2)
+// console.log(num1/num2)
+
+
+// let num = Number(prompt("Enter any number"))
+// const guessNumber = 5;
+
+// while(num !== guessNumber){
+//     num = Number(prompt("The guess number is wrong please enter again"))
+// }
+
+// console.log("Congratulation your guess number is correct!")
+
+
+// let num = Number(prompt("Enter any number"))
+// let sum = 0;
+
+// while(sum < 100){
+//     num = Number(prompt("Enter the number upto 100"))
+//     sum += num;
+// }
+
+// console.log("Your number is greater than 100 :", sum)
