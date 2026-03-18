863f08fa742e38f261ad609d0a3c60c61b8c85c5
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-18 12:00:00 +0500

    dom

--- javaScript/task 5/script.js
+++ javaScript/task 5/script.js
@@ -1,2 +1,6 @@
-const heading = document.getElementsByTagName("h1")
-console.log(heading[0])
+const root = document.getElementsByTagName("html")
+console.log(root[0])
+
+// const heading = document.getElementsByTagName("h1")
+// console.log(heading[0])
+
