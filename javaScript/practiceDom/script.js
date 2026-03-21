0d26619de2ceb5ea8dd12817370bec01b2abf12d
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-21 12:00:00 +0500

    DOM

--- javaScript/practiceDom/script.js
+++ javaScript/practiceDom/script.js
@@ -8,13 +8,11 @@ createDiv.style.display = "grid"
 createDiv.style.gridTemplateColumns = "auto auto"
 createDiv.setAttributeNode(createAtt)
 
+const color = ["red", "yellow", "gray", "aqua"]
 for(let i = 0; i < 4; i++){  
 const createChildDiv = document.createElement("div")
 const createChildAt = document.createAttribute("box")
-if(i == 0) createChildDiv.style.backgroundColor = "yellow"
-if(i == 1) createChildDiv.style.backgroundColor = "red"
-if(i == 2) createChildDiv.style.backgroundColor = "aqua"
-if(i == 3) createChildDiv.style.backgroundColor = "gray"
+createChildDiv.style.backgroundColor = color[i]
 createChildDiv.style.border = "1px solid black"
 createChildDiv.style.height = "100px"
 createChildDiv.style.width = "100px"
