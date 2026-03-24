002c260a109fbcdd0322bd60ada6e218910dfaa2
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-24 09:31:53 +0500

    DOM

---
+++ javaScript/dom/script.js
@@ -0,0 +1,51 @@
+// const root = document.getElementsByTagName("html")
+// console.log(root[0])
+
+// const title = document.title
+// console.log(title)
+
+// const pageUrl = document.URL
+// console.log(pageUrl)
+
+// const head = document.head 
+// console.log(head.innerHTML)
+
+// const links = document.links
+// console.log(links)
+
+// const heading = document.getElementsByTagName("h1")
+// console.log(heading[0])
+
+// const className = document.getElementsByClassName("para")
+// console.log(className[0].innerHTML)
+
+// const idName = document.getElementById("para")
+// console.log(idName.innerHTML)
+
+// const byQuery = document.querySelector("#para")
+// console.log(byQuery.innerHTML)
+
+// const byQueryAll = document.querySelectorAll(".para")
+// console.log(byQueryAll)
+
+// const heading = document.querySelector("#para")
+// heading.innerHTML = "i am not a bussiness man"
+
+// const changeAttvalue = document.querySelectorAll(".para")
+// changeAttvalue[2].setAttribute("class", "myPara")
+// const myParag = document.querySelector(".myPara")
+// myParag.innerHTML = "hello world"
+// myParag.style.color = "blue"
+// myParag.style.fontSize = "24px"
+
+// const bodyTag = document.getElementsByTagName("body")
+// const newDiv = document.createElement("div")
+// const heaing1 = document.createElement("h1")
+// heaing1.innerHTML = "Hellow meri jan!"
+// bodyTag[0].appendChild(newDiv)
+// newDiv.appendChild(heaing1)
+
+// const createEle = document.createElement("h1")
+// createEle.innerText = "hello meri jan"
+// const container = document.getElementById("cont")
+// container.appendChild(createEle)
---
+++ javaScript/practiceDom/script.js
@@ -0,0 +1,6 @@
+const createDiv = document.createElement("div")
+const createAtt = document.createAttribute("id")
+createAtt.
+con
+const bodyTag = document.getElementsByTagName("body")
+bodyTag[0].appendChild(createDiv)
