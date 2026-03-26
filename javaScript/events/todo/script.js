08f83447459e3483dd5d645d6e4b456a30e5a0f0
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-26 12:00:00 +0500

    todo with events

--- javaScript/events/todo/script.js
+++ javaScript/events/todo/script.js
@@ -1,9 +1,19 @@
 const todo = document.getElementById("todo")
 const btn1 = document.getElementById("btn1")
 const ol = document.getElementById("list")
-btn1.addEventListener("click", () => {
+
+function addTodo() {
     const value = todo.value
     const li = document.createElement("li")
     li.innerHTML = value
     ol.appendChild(li)
+}
+
+todo.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        addTodo()
+    }
+})
+btn1.addEventListener("click", () => {
+    addTodo()
 })
