aae0e01588cbe85cbf97f927b6dd740636838ac6
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-28 15:49:57 +0500

    todo

--- javaScript/events/todo/script.js
+++ javaScript/events/todo/script.js
@@ -1,23 +1,27 @@
-const todo = document.getElementById("todo")
-const btn1 = document.getElementById("btn1")
-const ol = document.getElementById("list")
+// const todo = document.getElementById("todo")
+// const btn1 = document.getElementById("btn1")
+// const ol = document.getElementById("list")
 
-function addTodo() {
-    const value = todo.value
-    const li = document.createElement("li")
-    li.innerHTML = value
-    value ? ol.appendChild(li) && (todo.value = "") : alert("please enter any value")
-}
+// function addTodo() {
+//     const value = todo.value
+//     const li = document.createElement("li")
+//     li.innerHTML = value
+//     value ? ol.appendChild(li) && (todo.value = "") : alert("please enter any value")
+// }
 
-todo.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        addTodo()
-    }
-})
-btn1.addEventListener("click", () => {
-    addTodo()
-})
+// todo.addEventListener("keypress", (event) => {
+//     if (event.key === "Enter") {
+//         addTodo()
+//     }
+// })
+// btn1.addEventListener("click", () => {
+//     addTodo()
+// })
 
+
+// spread operator
 let array = [1,2,3,4,5]
-let newArray = [...array, array]
+let newArray = [...array, 6,8]
 console.log(newArray)
+
+console.log(...newArray)
