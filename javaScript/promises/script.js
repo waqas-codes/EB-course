67ab3dcf98d5ad83c5ca020240e916a9ac874e12
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-30 17:47:39 +0500

    async await

--- javaScript/promises/script.js
+++ javaScript/promises/script.js
@@ -1,19 +1,20 @@
 const table = document.getElementById("table")
 
 
-async function add(){
+async function add() {
     let end_point = await fetch("https://jsonplaceholder.typicode.com/users")
     let data = await end_point.json()
     // console.log(data)
 
-
-    for(let i = 0; i < data.length; i++){
+    const arr = ["id", "name", "email"]
+    for (let i = 0; i < data.length; i++) {
         const tr = document.createElement("tr")
-        const td = document.createElement("td")
+        for (let j = 0; j < 3; j++) {
+            const td = document.createElement("td")
+            td.innerHTML = await data[i][arr[j]]
+            tr.appendChild(td)
+        }
         table.appendChild(tr)
-        td.innerHTML = data[i].id
-        tr.appendChild(td)
     }
 }
-
 add()
