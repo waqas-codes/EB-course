a9031d8b06c29f3b57bcd46101d0da7fb2196b7f
waqas-codes <muhammadwaqas96845@gmail.com>
2026-03-30 16:18:01 +0500

    async await

--- javaScript/promises/script.js
+++ javaScript/promises/script.js
@@ -1,1 +1,17 @@
-console.log("hello")
+console.log("9")
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")
+add()
+console.log("6")
+console.log("7")
+console.log("8")
+
+async function add(){
+    let end_point = await fetch("https://jsonplaceholder.typicode.com/users")
+    let data = await end_point.json()
+    console.log(data)
+}
+
