20aa0850ca3366184e8af058082c78473f3abd7d
waqas-codes <muhammadwaqas96845@gmail.com>
2026-04-01 16:13:38 +0500

    quiz task

--- javaScript/TaskSEctionD/script.js
+++ javaScript/TaskSEctionD/script.js
@@ -15,17 +15,17 @@
 // })
 
 
-// let arr = ["apple", "banana", "mango"]
-// for(let i = arr.length -1 ; i >= 0; i--){
-//     console.log(arr[i])
-// }
-
-
-let arr = [1, 5, 8, 19, 3, 66, 29]
-let count = 0;
-for(let i = 0; i < arr.length; i++){
-    if(arr[i] > 10){
-        count++;
-    }
+let arr = ["apple", "banana", "mango"]
+for(let i = arr.length -1 ; i >= 0; i--){
+    console.log(arr[i])
 }
-console.log(count)
+
+
+// let arr = [1, 5, 8, 19, 3, 66, 29]
+// let count = 0;
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > 10){
+//         count++;
+//     }
+// }
+// console.log(count)
