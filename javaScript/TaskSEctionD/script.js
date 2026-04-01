94bc7925bcd4781d1016a6113506dbc61fc160a6
waqas-codes <muhammadwaqas96845@gmail.com>
2026-04-01 16:14:11 +0500

    quiz task

--- javaScript/TaskSEctionD/script.js
+++ javaScript/TaskSEctionD/script.js
@@ -2,16 +2,16 @@
 //     console.log(i)
 // }
 
-// for(let i = 1; i <= 20; i++){
-//     if(i %2 == 0){
-//         console.log(i)
-//     }
-// }
+for(let i = 1; i <= 20; i++){
+    if(i %2 == 0){
+        console.log(i)
+    }
+}
 
 
-let arr = ["apple", "banana", "mango"]
-arr.forEach(Element => {
-    console.log(Element)
+// let arr = ["apple", "banana", "mango"]
+// arr.forEach(Element => {
+//     console.log(Element)
 })
 
 
