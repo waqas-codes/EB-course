a28852042a84abc9412dc564e0acb416513639b7
waqas-codes <muhammadwaqas96845@gmail.com>
2026-04-17 17:15:39 +0500

    react assignment

--- react/src/components/Users.jsx
+++ react/src/components/Users.jsx
@@ -2,25 +2,27 @@ import React, { useEffect, useState } from 'react'
 
 const Users = () => {
   const [users, setUser] = useState([])
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     const getData = async () => {
       const apiData = await fetch('https://jsonplaceholder.typicode.com/users')
       const fakeData = await apiData.json()
       setUser(fakeData)
       console.log(fakeData)
+      setLoading(false)
     }
     getData()
   }, [])
   return (
     <div className='h-125 w-screen flex items-center justify-center bg-gray-700'>
         {
-          users ? <div className='w-120 h-fit bg-white/20 backdrop-blur-lg border border-white/90 rounded-2xl p-4 shadow-lg'>
+          loading ? <h1 className='text-white'>loading...</h1> : <div className='w-120 h-fit bg-white/20 backdrop-blur-lg border border-white/90 rounded-2xl p-4 shadow-lg'>
             {
               users.map((user) => 
               <h1 className='text-xl text-white'>{user.name}</h1>
             )
             }
-        </div> : <h1>loading...</h1>
+        </div>
         }
     </div>
   )
